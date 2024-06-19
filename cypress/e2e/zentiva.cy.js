250f4679b3e94226a9091d4678e5acd0c9a653df
nivada12 <aminesrasra1@gmail.com>
2024-06-19 12:43:36 +0200

    Project added

--- cypress/e2e/zentiva.cy.js
+++ cypress/e2e/zentiva.cy.js
@@ -2,4 +2,15 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://front.qa.ip-label.io/restitution/#/login')
   })
+  
 })
+
+describe('test equal',()=>
+{
+  it('test equal',()=>
+  {
+    expect(true).to.equal(true)
+  }
+  )
+}
+)
